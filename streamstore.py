232d85d5c1481622d477ee3a73154d9d9eba74a8
David Testé <soonum@gnu.org>
2016-09-23 13:08:25 +0200

    Changed project name
    Project name has changed from ABYSS to HUBAngl.

--- streamstore.py
+++ streamstore.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3.4
 # -*- coding: utf-8 -*-
 
-# This file is part of ABYSS.
-# ABYSS Broadcast Your Streaming Successfully 
+# This file is part of HUBAngl.
+# HUBAngl Uses Broadcaster Angle 
 #
-# ABYSS is free software: you can redistribute it and/or modify
+# HUBAngl is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ABYSS is distributed in the hope that it will be useful,
+# HUBAngl is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ABYSS.  If not, see <http://www.gnu.org/licenses/>.
+# along with HUBAngl.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Copyright (c) 2016 David Testé
 
